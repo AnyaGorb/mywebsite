55031d954e458f7430cab2457ddbd046cf7ec9e0
AnyaGorb <anna2211gorb@gmail.com>
2024-01-28 21:09:45 +0300

    design and content

--- main/views.py
+++ main/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     data = {
-        'title': 'Главная страница',
-        'values': ['Anya', 'fairy', '123']
+        'title': 'Главная',
     }
     return render(request, 'main/index.html', data)
 
